42acba368e9b1c869d43877a2c7e93bdf1b73ef1
giancroci <giancroci5@gmail.com>
2026-05-06 13:59:12 -0300

    Registro, Login y auth

---
+++ Services/Interfaces/IUsuarioService.cs
@@ -0,0 +1,15 @@
+﻿using DTOs;
+using DTOs.ViewModels;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Interfaces
+{
+    public interface IUsuarioService
+    {
+        Task Registrar(RegistroViewModel vm);
+        Task<UsuarioLoginDto> Login(string email, string password);
+    }
+}

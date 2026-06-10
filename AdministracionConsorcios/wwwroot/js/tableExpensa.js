36582ea8543919f59cfa930f3900d2e41de912eb
giancroci <giancroci5@gmail.com>
2026-06-10 18:47:10 -0300

    fix table expensas

---
+++ AdministracionConsorcios/wwwroot/js/tableExpensa.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    $('#tablaExpensas').DataTable({
+        ajax: {
+            url: '/Expensa/GetExpensas?Id=' + consorcioId,
+            type: 'GET',
+            dataSrc: 'data'
+        },
+        columns: [
+            { data: 'año' },
+            { data: 'mes' },
+            { data: 'gastoMes', render: data => '$' + data.toLocaleString('es-AR') },
+            { data: 'montoXUnidad', render: data => '$' + data.toLocaleString('es-AR') }
+        ],
+        ordering: false,
+        language: {
+            url: '//cdn.datatables.net/plug-ins/1.13.7/i18n/es-ES.json'
+        }
+    });
+});
